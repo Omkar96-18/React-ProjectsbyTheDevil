f47910da62c8adb6e2e911ee0e4dbff07b1e4a5d
The Devil <omkarp.0906@gmail.com>
2025-08-27 00:10:54 +0530

    commit

--- basicsReact01/src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Post from "./Post";
-import UserCard from "./UserCard";
-import DarkMode from "./DarkMode/DarkMode";
-
-export {
-    UserCard,
-    Post,
-    DarkMode,
-}
--- basicsReact01/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import Login from "./Login/Login"
-import TodoList from "./TodoList/TodoList"
-
-export {
-    Login,
-    TodoList,
-}
